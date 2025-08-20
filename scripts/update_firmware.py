64ba5790c9d33561ab0e3472954b16eb0a9f12a0
Jeremy Mitchell <jmitchell@exoterracorp.com>
2025-08-20 16:45:59 -0600

    JTM - standardize on double dashes and remove legacy script

--- scripts/update_firmware.py
+++ scripts/update_firmware.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument('system_id', action='store', type=str, help='The System Id for the connection usually 0x22.',
                         default=0x22)
     parser.add_argument('image_file', action='store', type=str, help='The SAM firmware image file.')
-    parser.add_argument('-v', action='store_true', help='Run just the verify and install option.')
+    parser.add_argument('--v', action='store_true', help='Run just the verify and install option.')
     args = parser.parse_args()
     try:
         args.system_id = int(args.system_id, 16)
