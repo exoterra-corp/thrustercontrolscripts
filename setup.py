752a4988871cc7c66d2135b2e3a138ea61db95d4
Jeremy Mitchell <jmitchell@exoterra.com>
2026-08-19 14:47:40 -0600

    JTM - upped the version to 3.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='thrustercontrolscripts',
-      version='3.0.2',
+      version='3.0.3',
       description='Python System Controller Support Library.',
       author='Jeremy Mitchell',
       author_email='jmitchell@exoterracorp.com'
